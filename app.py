53d09732b77adda1006a7f0109e19f60546b7c6c
Edward Ang <wei.hen.ang@gmail.com>
2024-01-10 17:20:46 +0800

    smtp_commit

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ mongo_client = MongoClient('mongodb://localhost:27017/')
 host_info = mongo_client['HOST']
 print ("Mongo host info:", host_info)
 
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.getenv('SMTP_USERNAME')
